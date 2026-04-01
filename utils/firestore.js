9c7805f0f19bcc66f7a76a4cc0e5ed594bb7561d
Oh-donghun <abkomp400@gmail.com>
2026-04-01 17:33:53 +0900

    firestore.js ADC 방식으로 변경 - serviceAccount.json 제거

--- utils/firestore.js
+++ utils/firestore.js
@@ -1,9 +1,4 @@
 ﻿const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccount.json');
-
-if (!admin.apps.length) {
-  admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
-}
 
 const db = admin.firestore();
 const FieldValue = admin.firestore.FieldValue;
